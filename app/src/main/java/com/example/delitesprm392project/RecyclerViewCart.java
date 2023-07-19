f9eae147c3b39cd75903568e874a85a26e83f86f
nhatlinhhh <linhphan.05012003@gmail.com>
2023-07-19 11:23:28 +0700

    update code cart layout

---
+++ app/src/main/java/com/example/delitesprm392project/RecyclerViewCart.java
@@ -0,0 +1,14 @@
+package com.example.delitesprm392project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclerViewCart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_cart);
+    }
+}
