d0640f1f8ae815dfce03fb79d98b9774bfc0b56f
RSShiroshin <99200472+RSShiroshin@users.noreply.github.com>
2023-07-20 21:11:23 +0700

    fire base

--- app/src/main/java/com/example/delitesprm392project/RecyclerView/ProductManager/ProductManagerRecycleView.java
+++ app/src/main/java/com/example/delitesprm392project/RecyclerView/ProductManager/ProductManagerRecycleView.java
@@ -1,23 +1,34 @@
 package com.example.delitesprm392project.RecyclerView.ProductManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.delitesprm392project.R;
 import com.example.delitesprm392project.model.Product;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductManagerRecycleView extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    ArrayList<Product> productRecycles;
+    List<Product> productRecycles;
     ProductManagerAdapter productAdapter;
+
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
 
     Button addProduct;
     EditText searchTxt;
@@ -41,8 +52,27 @@ public class ProductManagerRecycleView extends AppCompatActivity {
         searchTxt = findViewById(R.id.viewRecycleProductManagerSearch);
         searchBtn = findViewById(R.id.viewRecycleProductManagerSearchBtn);
 
-        AddList();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Products");
 
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                productRecycles = new ArrayList<>();
+                for(DataSnapshot data : dataSnapshot.getChildren()) {
+                    Product model = data.getValue(Product.class);
+                    productRecycles.add(model);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("dwdaw ","ddd");
+            }
+        });
+
+        AddList();
 
         productAdapter = new ProductManagerAdapter(productRecycles, ProductManagerRecycleView.this);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
